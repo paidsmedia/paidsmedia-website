5fc25c7e4d69d6c49442d9e573d47e81ee71b186
paidsmedia <paidsmedia@gmail.com>
2026-06-21 15:18:12 +0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PaidMediaAgencyWebsite from './PaidMediaAgencyWebsite'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PaidMediaAgencyWebsite />
+  </React.StrictMode>
+)
