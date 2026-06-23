fadbd542e5a3d09fc44a8f32aff9a2a82f96c5bf
paidsmedia <paidsmedia@gmail.com>
2026-06-23 10:17:45 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PaidMediaAgencyWebsite from './PaidMediaAgencyWebsite'
-import ThankYouPage from './ThankYouPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PaidMediaAgencyWebsite />} />
-        <Route path="/thank-you" element={<ThankYouPage />} />
-      </Routes>
-    </BrowserRouter>
+    <PaidMediaAgencyWebsite />
   </React.StrictMode>
 )
